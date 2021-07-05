6aaa7e597de65b4e886041f6f271eca1c16ad9f1
Julio Junior <julio.junior@xpi.com.br>
2021-07-05 11:20:05 -0300

    hotfix path to SPM

--- Package.swift
+++ Package.swift
@@ -19,9 +19,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConfigurationProvider",
-            dependencies: []),
+            path: "ConfigurationProvider/ConfigurationProvider"),
         .testTarget(
             name: "ConfigurationProviderTests",
-            dependencies: ["ConfigurationProvider"]),
+            dependencies: ["ConfigurationProvider"],
+            path: "ConfigurationProvider/ConfigurationProviderTests"),
     ]
 )

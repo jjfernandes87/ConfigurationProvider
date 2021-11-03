9712c7bf3a218d50f51b3167b385786e45e0b64e
Valmir Massoni <valmir.massoni@xpi.com.br>
2021-11-03 15:03:01 -0300

    Remove Pods

--- Example/Pods/Target Support Files/ConfigurationProvider/ConfigurationProvider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ConfigurationProvider : NSObject
-@end
-@implementation PodsDummy_ConfigurationProvider
-@end
--- Example/Pods/Target Support Files/Pods-ConfigurationProvider_Tests/Pods-ConfigurationProvider_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ConfigurationProvider_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ConfigurationProvider_Tests
-@end
